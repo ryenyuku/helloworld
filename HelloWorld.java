8280ffe20ec92abf0a3aaf7e30c42a0803873663
リェンーゆく <teamworks1732@gmail.com>
2021-01-06 13:31:21 +0700

    more unoptimized way

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,36 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class HelloWorld {
 
     public static void main(String args[]) {
-        ArrayList<String> text = new ArrayList<String>();
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+        
+        ArrayList<Object> text = new ArrayList<>();
+        Map<String, Object> nocontext = new HashMap<>();
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
+        nocontext = new HashMap<>(nocontext);
         
         text.add(getH());
         text.add(getE());
@@ -18,56 +45,157 @@ public class HelloWorld {
         text.add(getD());
         text.add(getEnd());
         
-        for (String theElement: text) {
-            System.out.print(theElement);
+        Collections.reverse(text);
+        Collections.reverse(text);
+        Collections.reverse(text);
+        Collections.reverse(text);
+        Collections.reverse(text);
+        Collections.reverse(text);
+        Collections.reverse(text);
+        Collections.reverse(text);
+        Collections.reverse(text);
+        Collections.reverse(text);
+        
+        ArrayList<Object> aaaaaaaaaaa = new ArrayList<>(text);
+        text.clear();
+        Collections.reverse(aaaaaaaaaaa);
+        Collections.reverse(aaaaaaaaaaa);
+        Collections.reverse(aaaaaaaaaaa);
+        Collections.reverse(aaaaaaaaaaa);
+        Collections.reverse(aaaaaaaaaaa);
+        Collections.reverse(aaaaaaaaaaa);
+        Collections.reverse(aaaaaaaaaaa);
+        Collections.reverse(aaaaaaaaaaa);
+        text.addAll(aaaaaaaaaaa);
+        
+        if (true) if (true) if (true) if (true) if (true) if (true) if (true) if (true) if (true) if (true) if (true) if (true) if (true) {
+            for (int j = 0; j < text.size(); j++) {
+                boolean h = true;
+                while(h) {
+                    System.out.print((String)((String)((String)((String)text.get(j)))));
+                    h = false;
+                }
+            }
         }
     }
     
     public static String getH() {
-         return "H";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+         return (String)((Object)"H");
     }
     
     public static String getE() {
-         return "e";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+         return (String)((Object)"e");
     }
     
     public static String getL1() {
-         return "l";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+         return (String)((Object)"l");
     }
     
     public static String getL2() {
-         return "l";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+         return (String)((Object)"l");
     }
     
     public static String getO() {
-         return "o";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+         return (String)((Object)"o");
     }
     
     public static String getBlank() {
-         return " ";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+         return (String)((Object)" ");
     }
     
     public static String getW() {
-         return "W";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+         return (String)((Object)"W");
     }
     
     public static String getO2() {
-         return "o";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+         return (String)((Object)"o");
     }
     
     public static String getR() {
-         return "r";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+         return (String)((Object)"r");
     }
     
     public static String getL3() {
-         return "l";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+         return (String)((Object)"l");
     }
     
     public static String getD() {
-         return "d";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+         return (String)((Object)"d");
     }
     
     public static String getEnd() {
-         return "!";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+         return (String)((Object)"!");
     }
 }
